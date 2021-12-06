32d26565a65e7a4d6cd076491653f6c2a65dec84
Szymon Wiszczuk <golota60@gmail.com>
2021-12-06 08:39:09 +0100

    rm comments

--- 6.js
+++ 6.js
@@ -1,4 +1,4 @@
-let arr = new Array(9).fill(0); //indexes are how many fishes there are
+let arr = new Array(9).fill(0);
 let input =
   `2,1,2,1,5,1,5,1,2,2,1,1,5,1,4,4,4,3,1,2,2,3,4,1,1,5,1,1,4,2,5,5,5,1,1,4,5,4,1,1,4,2,1,4,1,2,2,5,1,1,5,1,1,3,4,4,1,2,3,1,5,5,4,1,4,1,2,1,5,1,1,1,3,4,1,1,5,1,5,1,1,5,1,1,4,3,2,4,1,4,1,5,3,3,1,5,1,3,1,1,4,1,4,5,2,3,1,1,1,1,3,1,2,1,5,1,1,5,1,1,1,1,4,1,4,3,1,5,1,1,5,4,4,2,1,4,5,1,1,3,3,1,1,4,2,5,5,2,4,1,4,5,4,5,3,1,4,1,5,2,4,5,3,1,3,2,4,5,4,4,1,5,1,5,1,2,2,1,4,1,1,4,2,2,2,4,1,1,5,3,1,1,5,4,4,1,5,1,3,1,3,2,2,1,1,4,1,4,1,2,2,1,1,3,5,1,2,1,3,1,4,5,1,3,4,1,1,1,1,4,3,3,4,5,1,1,1,1,1,2,4,5,3,4,2,1,1,1,3,3,1,4,1,1,4,2,1,5,1,1,2,3,4,2,5,1,1,1,5,1,1,4,1,2,4,1,1,2,4,3,4,2,3,1,1,2,1,5,4,2,3,5,1,2,3,1,2,2,1,4`
     .split(",")
@@ -13,12 +13,12 @@ const DAYS = 256; // <= PT2 // change to 80 FOR PT.1
 const countElems = (arr) => arr.reduce((acc, e, i) => acc + e, 0);
 
 const shift = (arr) => {
-  let newArr = arr.slice(1); //rem first elem
+  let newArr = arr.slice(1);
 
   return newArr;
 };
 
-const calcAndAdd2 = (arr, prevVal) => {
+const calcAndAdd2 = (arr) => {
   let oldFishes = arr[0];
 
   let shifted = shift(arr);
@@ -30,7 +30,7 @@ const calcAndAdd2 = (arr, prevVal) => {
 let lanternFish;
 let leng = 0;
 
-new Array(DAYS).fill().map((e, i, thisArr) => {
+new Array(DAYS).fill().map(() => {
   input = calcAndAdd2(input);
 });
 
